b46fad6a1b4e97210736adf9114a210fa6d58adc
effrancina <f.v.menichetti@gmail.com>
2023-01-23 15:30:21 +0000

    Initial commit

---
+++ src/main/java/CreditCard.java
@@ -0,0 +1,13 @@
+public class CreditCard extends BankCard {
+
+    public CreditCard(String cardNumber, String expiryDate, int securityNumber) {
+        super(cardNumber, expiryDate, securityNumber);
+
+    }
+
+
+
+    public String scan() {
+        return "Payment Successful";
+    }
+}
